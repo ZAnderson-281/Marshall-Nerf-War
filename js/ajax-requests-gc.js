aa5709a16438db952e8a1309982d7f59ff2ac5c0
Zac Anderson <azac281@gmail.com>
2019-09-12 12:57:44 -0500

    Add interactivity to Game component page

--- js/ajax-requests-gc.js
+++ js/ajax-requests-gc.js
@@ -21,6 +21,29 @@ request.onreadystatechange = function () {
       data = JSON.parse(request.responseText);
       // Log the data to the console
       // console.log(data);
+
+      // for however many items in object create a table element
+      for (let i = 0; i<data.length;i+=1){
+        // table html code
+        registry_table_element = `
+        <td>${i+1}</td>
+        <td>${data[i].name}</td>
+        <td>${data[i].gameName}</td>
+        <td>${data[i].email}</td>
+        <td>${data[i].team}</td>
+        <td>${data[i].teamRanking}</td>
+        <td>${data[i].paid}</td>
+        <td>${data[i].timedOut}</td>
+        <td>${data[i].kills}</td>
+        <td>${data[i].bounty}</td>
+        <td>${data[i].killedBy}</td>
+        <td>${data[i].killedBy2}</td>
+          `;
+        // log the html into the console
+        //console.log(registry_table_element);
+        document.getElementsByClassName("mainTable")[0].insertRow(i+1).innerHTML = registry_table_element;
+      }
+
     }else{
       //alert the error
       alert(request.status);
