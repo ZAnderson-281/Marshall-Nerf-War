6b0e439e26fd9db6d1f7b4acba85e63751d5bdd3
Zac Anderson <email@example.comazac281@gmail.com>
2020-03-11 00:13:43 -0500

    Added a server for api and mongo talking

---
+++ server/api/users.js
@@ -0,0 +1,52 @@
+users = [
+    {
+        "id": 1,
+        "name": "Jackson Difter",
+        "email": "jack@domain.com",
+        "paid": "Paid",
+        "gameName": "Jack",
+        "team": "",
+        "teamRanking": "",
+        "status": "Alive",
+        "timedOut": "No",
+        "bounty": "None",
+        "kills": 3,
+        "killsName": "",
+        "killedBy": "",
+        "killedBy2": ""
+      },
+      {
+        "id": 2,
+        "name": "Sam Difter",
+        "email": "jack@domain.com",
+        "paid": "Paid",
+        "gameName": "Jack",
+        "team": "",
+        "teamRanking": "",
+        "status": "Alive",
+        "timedOut": "No",
+        "bounty": "None",
+        "kills": 3,
+        "killsName": "",
+        "killedBy": "",
+        "killedBy2": ""
+      },
+      {
+        "id": 3,
+        "name": "Sarah Difter",
+        "email": "jack@domain.com",
+        "paid": "Paid",
+        "gameName": "Jack",
+        "team": "",
+        "teamRanking": "",
+        "status": "Alive",
+        "timedOut": "No",
+        "bounty": "None",
+        "kills": 3,
+        "killsName": "",
+        "killedBy": "",
+        "killedBy2": ""
+      }
+]
+
+module.exports = users;
