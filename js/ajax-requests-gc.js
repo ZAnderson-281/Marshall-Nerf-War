d7206ad1c0f971c9a9e19145c5521b07ecd43b3c
Zac Anderson <email@example.comazac281@gmail.com>
2020-03-11 00:30:01 -0500

    Revert "Added a server for api and mongo talking"
    This reverts commit 6b0e439e26fd9db6d1f7b4acba85e63751d5bdd3.

--- js/ajax-requests-gc.js
+++ js/ajax-requests-gc.js
@@ -1,86 +1,293 @@
-const getDataFromForm = () => {
-    let userData = {
-        id: 3,
-        name: null,
-        email: null,
-        paid: null,
-        gameName: null,
-        team: null,
-        teamRanking: null,
-        status: null,
-        timedOut: null,
-        bounty: null,
-        kills: null,
-        killsName: null,
-        killedBy: null,
-        killedBy2: null,
-        notes: null
-      };
 
-    const input = document.querySelectorAll('input');
+//ON Menu Select Change
+function changeJSON(clicked_id){
 
-    console.log(input);
+  //Create the variables
+  let request;
+  let data;
+  let registry_table_element;
+  let table_header;
+  let dead;
+  let alive;
+  // create the request
+  request = new XMLHttpRequest();
 
-    input.forEach((element, index) => {
-        if (element.value == ''){return;}
+  if(clicked_id === "main-reg"){
+    //creates the callback function
+    request.onreadystatechange = function () {
 
-        switch (index){
-            case 1:
-                userData.name = element.value;
-                element.value = '';
-                break;
-            case 2:
-                userData.email = element.value;
-                element.value = '';
-                break;
-            case 3:
-                userData.gameName = element.value;
-                element.value = '';
-                break;
-            case 4:
-                userData.kills = element.value;
-                element.value = '';
-                break;
-            case 5:
-                userData.rank = element.value;
-                element.value = '';
-                break;
-            case 6:
-                userData.status = element.checked;
-                element.checked = false;
-                break;
-            case 7:
-                userData.paid = element.checked;
-                element.checked = false;
-                break;
-            case 8:
-                userData.killedBy = element.value;
-                element.value = '';
-                break;
-            case 9:
-                userData.notes = document.querySelector('#otherNotes').value;
-                document.querySelector('#otherNotes').value = '';
-                break;
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Player Name</th>
+          <th>Email</th>
+          <th>Team</th>
+          <th>Team Rank</th>
+          <th>Paid</th>
+          <th>Timed Out</th>
+          <th>Kills</th>
+          <th>Bounty</th>
+          <th>Killed By</th>
+          <th>Killed By 2</th>
+          `
+          document.getElementsByClassName('reg-thead')[1].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          data = JSON.parse(request.responseText);
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>${i+1}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].gameName}</td>
+            <td>${data[i].email}</td>
+            <td>${data[i].team}</td>
+            <td>${data[i].teamRanking}</td>
+            <td>${data[i].paid}</td>
+            <td>${data[i].timedOut}</td>
+            <td>${data[i].kills}</td>
+            <td>${data[i].bounty}</td>
+            <td>${data[i].killedBy}</td>
+            <td>${data[i].killedBy2}</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementsByClassName("mainTable")[0].insertRow(i+1).innerHTML = registry_table_element;
+          }
+
+        }else{
+          //alert the error
+          alert(request.status);
         }
-    })
-    console.log(userData);
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
 
-    fetch('http://localhost:5000/api/users',{
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
-        },
-        method: 'POST',
-        body: JSON.stringify(userData)
-    })
+    // Send the request to the server
+    request.send();
+  }else if (clicked_id === "war-reg") {
+    //creates the callback function
+    request.onreadystatechange = function () {
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Rank</th>
+          <th>Player Name</th>
+          <th>Team</th>
+          <th>Team Rank</th>
+          <th>Timed Out</th>
+          <th>Kills</th>
+          <th>Bounty</th>
+          <th>Killed By</th>
+          <th>Killed By 2</th>
+          `
+          document.getElementsByClassName('reg-thead')[1].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+
+          // Parse the json data into javascript
+          data = JSON.parse(request.responseText);
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>${i+1}</td>
+            <td>${data[i].gameName}</td>
+            <td>${data[i].team}</td>
+            <td>${data[i].teamRanking}</td>
+            <td>${data[i].paid}</td>
+            <td>${data[i].timedOut}</td>
+            <td>${data[i].kills}</td>
+            <td>${data[i].killedBy}</td>
+            <td>${data[i].killedBy2}</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementsByClassName("mainTable")[0].insertRow(i+1).innerHTML = registry_table_element;
+          }
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }else if (clicked_id === "team-reg") {
+    //creates the callback function
+    request.onreadystatechange = function () {
+
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Team</th>
+          <th>Player Name</th>
+          <th>Status</th>
+          <th>Team Rank</th>
+          <th>Kills</th>
+          <th>Killed By</th>
+          <th>Killed By 2</th>
+          <th>Timed Out</th>
+          `
+          document.getElementsByClassName('reg-thead')[1].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          data = JSON.parse(request.responseText);
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>${data[i].team}</td>
+            <td>${data[i].gameName}</td>
+            <td>${data[i].status}</td>
+            <td>${data[i].teamRanking}</td>
+            <td>${data[i].kills}</td>
+            <td>${data[i].killedBy}</td>
+            <td>${data[i].killedBy2}</td>
+            <td>${data[i].timedOut}</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementsByClassName("mainTable")[0].insertRow(i+1).innerHTML = registry_table_element;
+          }
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }else if (clicked_id === "vote-reg") {
+    //creates the callback function
+    request.onreadystatechange = function () {
+
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Date</th>
+          <th>Type</th>
+          <th>Description</th>
+          `
+          document.getElementsByClassName('reg-thead')[1].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          data = JSON.parse(request.responseText);
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>${i+1}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].gameName}</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementsByClassName("mainTable")[0].insertRow(i+1).innerHTML = registry_table_element;
+          }
+
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }else{
+    //creates the callback function
+    request.onreadystatechange = function () {
+
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Player Name</th>
+          <th>Email</th>
+          <th>Team</th>
+          <th>Team Rank</th>
+          <th>Paid</th>
+          <th>Timed Out</th>
+          <th>Kills</th>
+          <th>Bounty</th>
+          <th>Killed By</th>
+          <th>Killed By 2</th>
+          `
+          document.getElementsByClassName('reg-thead')[1].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          data = JSON.parse(request.responseText);
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>${i+1}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].gameName}</td>
+            <td>${data[i].email}</td>
+            <td>${data[i].team}</td>
+            <td>${data[i].teamRanking}</td>
+            <td>${data[i].paid}</td>
+            <td>${data[i].timedOut}</td>
+            <td>${data[i].kills}</td>
+            <td>${data[i].bounty}</td>
+            <td>${data[i].killedBy}</td>
+            <td>${data[i].killedBy2}</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementsByClassName("mainTable")[0].insertRow(i+1).innerHTML = registry_table_element;
+          }
+
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }
 }
-
-// MAIN FUNCTION
-const GAME_DATA_SUBMIT = () => {
-    const subButton = document.querySelector('#submitCompCreator');
-
-    subButton.addEventListener('click', getDataFromForm);
-
-}
-
-GAME_DATA_SUBMIT();
+document.pageload(changeJSON());
--- js/ajax-requests.js
+++ js/ajax-requests.js
@@ -1,5 +1,258 @@
-const GAME_DATA_SUBMIT = () => {
-    alert('s');
-}
 
-GAME_DATA_SUBMIT();
+//ON Menu Select Change
+function changeJSON(clicked_id){
+  //Create the variables
+  let request;
+  let data;
+  let registry_table_element;
+  let table_header;
+  let dead = 0;
+  let alive = 0;
+  let sortKills = [];
+  let sortNames = [];
+  let sortID = [];
+
+  // create the request
+  request = new XMLHttpRequest();
+
+  if (clicked_id === "ind-reg" || clicked_id === "playerInfo") {
+    //creates the callback function
+    request.onreadystatechange = function () {
+
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Rank</th>
+          <th>Player Name</th>
+          <th>Status</th>
+          <th>Kills</th>
+          `
+          document.getElementsByClassName('reg-thead')[0].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          dataraw = JSON.parse(request.responseText);
+
+          let data = rawdata.sort(function(a,b){return b.kills - a.kills});
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>${i+1}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].status}</td>
+            <td>${data[i].kills}</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementById("registry").insertRow(i+1).innerHTML = registry_table_element;
+
+            // sort
+            if(data[i].status === "Alive"){
+              alive += 1;
+            }else if (data[i].status === "Dead"){
+              dead += 1;
+            }
+          }
+
+          document.getElementById('playerCountAlive').innerHTML = "Alive: "+alive;
+          document.getElementById('playerCountDead').innerHTML = "Dead: "+dead;
+          document.getElementById('playerCountAll').innerHTML = "Current Player Count: "+data.length;
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }else if(clicked_id == "vote-reg"){
+    //creates the callback function
+    request.onreadystatechange = function () {
+
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Date</th>
+          <th>Name</th>
+          <th>Ruleings</th>
+          <th>About</th>
+          `
+          document.getElementsByClassName('reg-thead')[0].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          dataraw = JSON.parse(request.responseText);
+
+          let data = rawdata.sort(function(a,b){return b.kills - a.kills});
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>NA</td>
+            <td>NA</td>
+            <td>NA</td>
+            <td>NA</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementById("registry").insertRow(i+1).innerHTML = registry_table_element;
+
+            // sort
+            if(data[i].status === "Alive"){
+              alive += 1;
+            }else if (data[i].status === "Dead"){
+              dead += 1;
+            }
+          }
+
+          document.getElementById('playerCountAlive').innerHTML = "Alive: "+alive;
+          document.getElementById('playerCountDead').innerHTML = "Dead: "+dead;
+          document.getElementById('playerCountAll').innerHTML = "Current Player Count: "+data.length;
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }else if (clicked_id === "team-reg" || clicked_id === "teamInfo") {
+    //creates the callback function
+    request.onreadystatechange = function () {
+
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Team</th>
+          <th>Team Rank</th>
+          <th>Player Name</th>
+          <th>Status</th>
+          <th>Kills</th>
+          `
+          document.getElementsByClassName('reg-thead')[0].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          rawdata = JSON.parse(request.responseText);
+
+          let data = rawdata.sort(function(a,b){return b.kills - a.kills});
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td>${data[i].team}</td>
+            <td>${data[i].teamRanking}</td>
+            <td>${data[i].name}</td>
+            <td>${data[i].status}</td>
+            <td>${data[i].kills}</td>
+              `;
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementById("registry").insertRow(i+1).innerHTML = registry_table_element;
+
+            // sort
+            if(data[i].status === "Alive"){
+              alive += 1;
+            }else if (data[i].status === "Dead"){
+              dead += 1;
+            }
+          }
+
+          document.getElementById('playerCountAlive').innerHTML = "Alive: "+alive;
+          document.getElementById('playerCountDead').innerHTML = "Dead: "+dead;
+          document.getElementById('playerCountAll').innerHTML = "Current Player Count: "+data.length;
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }else{
+    //creates the callback function
+    request.onreadystatechange = function () {
+
+      // if the server has returned a response
+      if (request.readyState === 4){
+        // Check if the status is 200 (ran with no issues) else return an ERROR message
+        if (request.status === 200){
+          table_header = `
+          <th>Rank</th>
+          <th>Player Name</th>
+          <th>Kills</th>
+
+          `
+          document.getElementsByClassName('reg-thead')[0].innerHTML = table_header;
+          document.getElementById('registryBody').innerHTML = '<tr></tr>';
+          // Parse the json data into javascript
+          rawdata = JSON.parse(request.responseText);
+
+          let data = rawdata.sort(function(a,b){return b.kills - a.kills});
+          // Log the data to the console
+          console.log(data[0]);
+
+          // for however many items in object create a table element
+          for (let i = 0; i<data.length;i+=1){
+            // table html code
+            registry_table_element = `
+            <td class="pt-2 pb-2">${i+1}</td>
+            <td class="pt-2 pb-2">${data[i].name}</td>
+            <td class="pt-2 pb-2">${data[i].kills}</td>
+ 
+              `;
+
+            // log the html into the console
+            console.log(registry_table_element);
+            document.getElementById("registry").insertRow(i+1).innerHTML = registry_table_element;
+
+            // sort
+            if(data[i].status === "Alive"){
+              alive += 1;
+            }else if (data[i].status === "Dead"){
+              dead += 1;
+            }
+          }
+
+          document.getElementById('playerCountAlive').innerHTML = "Alive: "+alive;
+          document.getElementById('playerCountDead').innerHTML = "Dead: "+dead;
+          document.getElementById('playerCountAll').innerHTML = "Current Player Count: "+data.length;
+
+          console.log(data.sort(function(a,b){return b.kills - a.kills}));
+
+        }else{
+          //alert the error
+          alert(request.status);
+        }
+      }
+    }
+    // Open the request as a get
+    request.open('GET', 'json/registryData.json');
+
+    // Send the request to the server
+    request.send();
+  }
+}
+document.pageload(changeJSON());
